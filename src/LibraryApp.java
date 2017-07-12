e119e57343a5aadeffc2d28c12407f6b41a7a49a
frnksinatra <ferasbaraka0@gmail.com>
2017-07-12 13:18:15 -0400

    second commit, saying hi chandler!

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -5,6 +5,7 @@ public class LibraryApp {
     public static void main(String[] args) {
         System.out.println("Hello Team!");
         System.out.println("Hi!");
+        System.out.println("What's up Chandler!");
     }
 
 }
