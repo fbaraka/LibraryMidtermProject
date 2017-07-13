0e726a864c580c2e578f5a6d893bb77a8ec927cf
chandlerkeyes <ckeyes4@emich.edu>
2017-07-12 22:25:13 -0400

    added abstract class in book class

---
+++ src/Books.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+/**
+ * Created by chand on 7/12/2017.
+ */
+public class Books extends Library {
+    private String title;
+    private String author;
+
+    @Override
+    public LocalDate returnDate() {
+        return super.returnDate();
+    }
+
+}
---
+++ src/Library.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by chand on 7/12/2017.
+ */
+public class Library {
+}
