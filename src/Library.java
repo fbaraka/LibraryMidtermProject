1e9434631fd30922d3d413c979097d375421db7b
chandlerkeyes <ckeyes4@emich.edu>
2017-07-14 11:10:36 -0400

    Additional Changes

--- src/Library.java
+++ src/Library.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Calendar;
+import java.util.Scanner;
 
 public class Library {
     ArrayList<LibraryItem> items = new <LibraryItem>ArrayList();
 
     public Library() {
         getList();
+    }
+
+    public ArrayList<LibraryItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<LibraryItem> items) {
+        this.items = items;
     }
 
     public ArrayList<LibraryItem> getList() {
@@ -16,11 +26,11 @@ public class Library {
             while (readLine != null) {
                 int i = 1;
                 String[] fileValues = readLine.split(",");
-                String titles = fileValues[0];
-                String authors = fileValues[1];
-                String availability = fileValues[2];
+                String title = fileValues[0];
+                String author = fileValues[1];
+                String status = fileValues[2];
 
-                LibraryItem bookObject = new LibraryItem(titles, authors, availability);
+                LibraryItem bookObject = new LibraryItem(title, author, status);
                 items.add(bookObject);
                 readLine = bufferedReader.readLine();
             }
@@ -33,30 +43,45 @@ public class Library {
 
     }
 
-     public void searchTitle(String userInput){
-        for(int i = 0; i < items.size(); i++){
-            if(items.get(i).getTitle().equalsIgnoreCase(userInput)){
+    public void searchTitle(String userInput) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getTitle().equalsIgnoreCase(userInput)) {
+                System.out.println(items.get(i).getTitle() + " by " + items.get(i).getAuthor());
+            }
+        }
+    }
+
+    public void searchAuthor(String userInput) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getAuthor().equalsIgnoreCase(userInput)) {
                 System.out.println(items.get(i).getTitle() + " by " + items.get(i).getTitle());
             }
         }
     }
-    public void searchAuthor(String userInput){
-         for(int i = 0; i <items.size(); i++){
-             if(items.get(i).getAuthors().equalsIgnoreCase(userInput)){
-                 System.out.println(items.get(i).getTitle() + " by " + items.get(i).getTitle());
-             }
-         }
+
+    public void checkOut(int userInput) {
+        System.out.println(items.get(userInput - 1).getTitle() + " is now checked out.");
+        System.out.println("Due Date: " + items.get(userInput - 1).getDueDate());
+        items.get(userInput - 1).setStatus("unavailable");
+        items.get(userInput - 1).getDueDate();
     }
 
-    public void checkOut(int userInput){
-        items.remove(userInput -1);
-        for (LibraryItem x : items) {
-            System.out.println(x);
-        }
+    public void returnBook(int userInput) {
+        System.out.println(items.get(userInput - 1).getTitle() + " is now returned.");
+        items.get(userInput - 1).setDueDate(null);
+        items.get(userInput - 1).setStatus("available");
     }
-    public void numberedList(){
-        for (int i = 1; i < items.size(); i++) {
-            System.out.println(i + ". " + items.get(i).getTitle());
+
+    public void numberedList() {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getStatus().equalsIgnoreCase("unavailable")) {
+                System.out.println(i + 1 + ". " + items.get(i).getTitle() + " "
+                        + items.get(i).getAuthor() + " " + items.get(i).getStatus() +
+                        " Due Date: " + items.get(i).getDueDate());
+            } else {
+                System.out.println((i + 1) + ". " + items.get(i).getTitle() + " " + items.get(i).getAuthor() + " "
+                        + items.get(i).getStatus());
+            }
         }
     }
 }
--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -1,11 +1,84 @@
+
+import java.util.Scanner;
 /**
  * Created by Feras on 7/12/2017.
  */
+
 public class LibraryApp {
     public static void main(String[] args) {
-        Books b = new Books();
-        b.returnDate();
+        Library library = new Library();
+        //Book book = new Book();
+        Scanner scan = new Scanner(System.in);
+        String yOrN = "";
+        System.out.println("Welcome to The Error's Library!");
+        System.out.println("Please select an option number from the menu");
+        do {
+            //System.out.println("Menu: \n1 -- Display library list\n2 -- Search the library\n3 -- Checkout item from library\n4 -- Return item to library\n5 -- End session");
+            int input = Validator.getInt(scan,"Menu: \n1 -- Display library list\n2 " +
+                    "-- Search the library\n3 -- Checkout item from library\n4 -- " +
+                    "Return item to library\n5 -- End session", 1, 5);
 
+            switch (input) {
+                case 1:
+                    System.out.println("Here is everything in our Library!\n"); //fill this in later with the list that has all items inside of it.
+                    library.numberedList();
+                    break;
+                case 2:
+                    library.numberedList();
+                    //scan.nextLine();
+                    System.out.println("\n" + "Search by: Title or Author");
+                    String userInput = scan.nextLine();
+                    userInput = titleOrAuthorValid(userInput);
+                    if (userInput.equalsIgnoreCase("title")) {
+                        System.out.print("Search: ");
+                        userInput = scan.nextLine();
+                        library.searchTitle(userInput);
+                    }
+                    else if (userInput.equalsIgnoreCase("author")){
+                        System.out.print("Search: ");
+                        userInput = scan.nextLine();
+                        library.searchAuthor(userInput);
+                    }
+                    break;
+                case 3:
+                    library.numberedList();
+                    int checkOut = Validator.getInt(scan,"\n Enter the number of the book " +
+                            "you would like to check out:", 1,12);
+
+                    library.checkOut(checkOut);
+                    break;
+
+                case 4:
+                    library.numberedList();
+                    int userReturn = Validator.getInt(scan,"\n Enter the number of the book " +
+                            "you would like to return:", 1,12);
+                    library.returnBook(userReturn);
+
+                    break;
+                case 5:
+                    System.out.println("Goodbye!");
+                    break;
+            }
+            //scan.nextLine();
+            System.out.println("\nContinue? Y/N");
+            yOrN = scan.nextLine();
+            yOrN = yOrNValid(yOrN);
+        }while(yOrN.equalsIgnoreCase("y"));
     }
-
+    public static String yOrNValid(String yOrN) {
+        Scanner keyboard = new Scanner(System.in);
+        while (!yOrN.equalsIgnoreCase("y") && !yOrN.equalsIgnoreCase("n")) {
+            System.out.println("Invalid Input. Please enter Y or N: ");
+            yOrN = keyboard.nextLine();
+        }
+        return yOrN;
+}
+    public static String titleOrAuthorValid(String userInput) {
+        Scanner keyboard = new Scanner(System.in);
+        while (!userInput.equalsIgnoreCase("title") && !userInput.equalsIgnoreCase("author")) {
+            System.out.println("Invalid Input. Please enter Title or Author: ");
+            userInput = keyboard.nextLine();
+        }
+        return userInput;
+    }
 }
