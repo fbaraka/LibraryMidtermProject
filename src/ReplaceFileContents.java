2abb6bbe69991d0767c2581b07dea779b38fc7c4
frnksinatra <ferasbaraka0@gmail.com>
2017-07-14 13:48:01 -0400

    started replace method for the our txt file

--- src/ReplaceFileContents.java
+++ src/ReplaceFileContents.java
@@ -1,9 +1,36 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 
 /**
  * Created by chand on 7/14/2017.
  */
 public class ReplaceFileContents {
+
+    String userInput;
+
+    public void replace() {
+
+        String originalFilename = "book.txt";
+        String tempFileName = "tmp_book.txt";
+
+        BufferedReader buff1 = null;
+        BufferedReader buff2 = null;
+
+        try {
+            buff1 = new BufferedReader(new FileReader(originalFilename));
+            buff2 = new BufferedReader(new FileReader(tempFileName));
+
+            String line;
+
+            while (line = buff1.readLine() != null){
+                if (line.contains(userInput)){
+
+                }
+            }
+        }catch (){
+
+        }
+    }
  
 }
